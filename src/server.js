380980af0ee92dbb1201f482fef3c6e9dc0fdf6a
Castanea02 <ahrhk2000@gmail.com>
2023-06-02 03:13:30 +0900

    GET, req, res

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
-import express from "express";
-
+import express from "express"; //use Express
+const PORT = 4000;
 const app = express();
 
-const PORT = 4000;
+//requset, response Arugument
+const handleHome = (req, res) => {
+  //GET root callback
+  return res.send("<h1>123</h1>"); //Home Response
+};
 
-const handleListening = () => console.log("Server Listening on port 4000 🔥");
+const handleLogin = (req, res) => {
+  return res.send("Login"); //Login Response
+};
 
-app.listen(PORT, handleListening);
+app.get("/", handleHome); //home(root) Request handleHome으로 처리
+app.get("/login", handleLogin); //Login Request handleLogin으로 처리
+
+const handleListening = () => {
+  //Server Callback
+  console.log(`Server Listening on port ${PORT} 🔥`);
+};
+
+app.listen(PORT, handleListening); //Server Listening
