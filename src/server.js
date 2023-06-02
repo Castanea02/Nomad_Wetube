d3c82e3c6bb8f32d641c52147e1084c703526e22
Castanea02 <ahrhk2000@gmail.com>
2023-06-02 16:02:46 +0900

    middleWare

--- src/server.js
+++ src/server.js
@@ -2,18 +2,35 @@ import express from "express"; //use Express
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+  //middleware
+  console.log(`${req.method}, ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not allowed</h1>");
+  }
+  console.log("Allow");
+  next();
+};
+
 //requset, response Arugument
 const handleHome = (req, res) => {
   //GET root callback
   return res.send("<h1>123</h1>"); //Home Response
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login"); //Login Response
+const handleProtected = (req, res, next) => {
+  return res.send("Private Check");
 };
 
+app.use(logger); //Use middleware All URL
+app.use(privateMiddleware);
+
 app.get("/", handleHome); //home(root) Request handleHome으로 처리
-app.get("/login", handleLogin); //Login Request handleLogin으로 처리
+app.get("/protected", handleProtected);
 
 const handleListening = () => {
   //Server Callback
