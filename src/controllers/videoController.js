0d8b12d4f3d8ba317aff29b48e220da000d77e9e
Castanea02 <ahrhk2000@gmail.com>
2023-06-07 03:50:21 +0900

    Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,37 @@
-const fakeUser = {
-  username: "CASTANEA",
-  loggedIn: true,
-};
+let ctrl_videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2m ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 10,
+    createdAt: "5m ago",
+    views: 20,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 3,
+    comments: 5,
+    createdAt: "10m ago",
+    views: 42,
+    id: 3,
+  },
+];
 
 export const home = (req, res) => {
-  const ctrl_videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2m ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2m ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2m ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser, ctrl_videos }); //render home.pug 2번째 인자는 템플릿에 보낼 변수 내용 지정
+  res.render("home", { pageTitle: "Home", ctrl_videos }); //render home.pug 2번째 인자는 템플릿에 보낼 변수 내용 지정
 }; //export home
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch Video" });
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = ctrl_videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 }; //export watch
 export const edit = (req, res) => {
   res.render("edit", { pageTitle: "Edit Video" });
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("upload", upload); //파라미터보다 항상 위에 위치할 것 (아니라면 id 파라미터로 인식)
-videoRouter.get("/:id(\\d+)", see); //id 파라미터 요청 (\\d+) -> 숫자만 정규표현식
+videoRouter.get("/:id(\\d+)", watch); //id 파라미터 요청 (\\d+) -> 숫자만 정규표현식
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
