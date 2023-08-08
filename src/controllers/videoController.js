3ab6712b8ede339a13b428406f5fc918ce05c394
Castanea02 <ahrhk2000@gmail.com>
2023-08-09 03:04:21 +0900

    Video Detail, Edit Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,16 +6,27 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos }); //render home.pug 2번째 인자는 템플릿에 보낼 변수 내용 지정
 };
 
-export const watch = (req, res) => {
-  const id = req.params.id;
-  return res.render("watch", { pageTitle: `Watching` });
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const videos = await Video.findById(id);
+  if (!videos) {
+    return res.render("404", { pageTitle: "Video not found" });
+  } else {
+    return res.render("watch", { pageTitle: videos.title, videos });
+  }
 }; //export watch
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", {
-    pageTitle: `Editing Video`,
-  });
+  const videos = await Video.findById(id);
+  if (!videos) {
+    return res.render("404", { pageTitle: "Video not found" });
+  } else {
+    return res.render("edit", {
+      pageTitle: `Edit ${videos.title}`,
+      videos,
+    });
+  }
 }; //export edit
 
 export const postEdit = (req, res) => {
@@ -32,18 +43,19 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body; //form name
-  await Video.create({
-    //save DB promice
-    //Create Video object
-    title,
-    description,
-    createAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-
-  return res.redirect("/");
+  try {
+    await Video.create({
+      //save DB promice
+      //Create Video object
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: `Upload Video`,
+      errorMessage: error._message,
+    });
+  }
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,8 +10,8 @@ import {
 const videoRouter = express.Router();
 
 //정규식이 없는 함수는 항상 위에 위치할 것 (아니라면 id 파라미터로 인식)
-videoRouter.get("/:id(\\d+)", watch); //id 파라미터 요청 (\\d+) -> 숫자만 정규표현식
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch); //id 파라미터 요청 (video ID) -> 숫자만 정규표현식
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
