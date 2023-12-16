94a4fad3480e74b00574b180cc4bba671a8fffad
Castanea02 <ahrhk2000@gmail.com>
2023-12-17 07:42:05 +0900

    CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,23 +1,43 @@
 import Video from "../models/Video"; //model 사용하기
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}); //promise
+  const videos = await Video.find({}).sort({ createdAt: "asc" }); //promise
   return res.render("home", { pageTitle: "Home", videos: videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id); //Video id로 찾기
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id); //Video id로 찾기
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id }); //Video true or false
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
+
+  await Video.findByIdAndUpdate(id, {
+    //Update
+    title,
+    description,
+    hashtags: Video.formatHashtags(hashtags), //,으로 구분 #으로 단어시작 Video 스키마에서 import formatHashtags
+  });
+
   return res.redirect(`/videos/${id}`);
 };
 
@@ -32,7 +52,7 @@ export const postUpload = async (req, res) => {
       //promise create DB Video
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags), //,으로 구분 #으로 단어시작 Video 스키마에서 import formatHashtags
     });
     return res.redirect("/");
   } catch (error) {
@@ -43,4 +63,24 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"), //정규표현식 검색
+      },
+    });
+    return res.render("search", { pageTitle: "Search", videos });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -2,14 +2,20 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now }, //default = DB 생성시 명시 안 해줘도 됨
-  hashtags: [{ type: String }], //Array
+  hashtags: [{ type: String, trim: true }], //Array
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema); //Video 모델 정의
